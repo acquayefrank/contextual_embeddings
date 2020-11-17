6ab0554d08ae55c59122746d067e5bd307fc6d6a
acquayefrank <acquayefrank@gmail.com>
2020-11-18 00:04:49 +0300

    changed code to work with all glove models

--- src/main.py
+++ src/main.py
@@ -51,149 +51,109 @@ def main(args):
         words = get_words()
     else:
         words = [args.word, ]
+    
+    if args.all_embeddings:
+        word_embeddings = ["GLOVE_6B_50D", "GLOVE_6B_100D", "GLOVE_6B_200D", "GLOVE_6B_300D"]
+    else:
+        word_embeddings = [args.word_embeddings]
 
-    file_path, num_features = embeddings.get(args.word_embeddings)
-    utils.WORD_EMBEDDINGS_MODEL = _load_glove_model(File=file_path)
+    total_number_of_word_embeddings = len(word_embeddings)
+    writer.add_text("Description", f"Number of word embeddings being trained: {total_number_of_word_embeddings}")
+    print(f"Total Number of word embeddings: {total_number_of_word_embeddings}")
 
-    writer.add_text("Description", f"Number of words being trained: {len(words)}")
-    total_number_of_words = len(words)
-    print(f"Total Number of words: {total_number_of_words}")
-    for word in words:
-        print(f"{word}: is being processed")
-        total_number_of_words = total_number_of_words - 1
-        print(f"{total_number_of_words} word(s) left to process after the current word")
-        args.word = word
-        writer.add_text("Description", f"Word being trained: {args.word}, Word Embeddings being used: {args.word_embeddings}")
+    for word_embedding in word_embeddings:
+        print(f"{word_embedding}: is being processed")
+        total_number_of_word_embeddings = total_number_of_word_embeddings - 1
+        print(f"{total_number_of_word_embeddings} word_embedding(s) left to process after the current word_embedding")
 
-        training_set = DatasetLoader(args.word, "train")
-        training_generator = torch.utils.data.DataLoader(training_set, **params)
+        args.word_embeddings = word_embedding
+        file_path, num_features = embeddings.get(args.word_embeddings)
+        utils.WORD_EMBEDDINGS_MODEL = _load_glove_model(File=file_path)
 
-        # currently not in use
-        # validation_set = DatasetLoader(args.word, "validation")
-        # validation_generator = torch.utils.data.DataLoader(validation_set, **params)
-
-        criterion = torch.nn.BCELoss(reduction="mean")
-
-        model = LogisticRegression(num_features, 1)
+        total_number_of_words = len(words)
+        writer.add_text("Description", f"Number of words being trained: {total_number_of_words}")
+        print(f"Total Number of words: {total_number_of_words}")
         
-        optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
+        for word in words:
+            print(f"{word}: is being processed")
+            total_number_of_words = total_number_of_words - 1
+            print(f"{total_number_of_words} word(s) left to process after the current word")
+            args.word = word
+            writer.add_text("Description", f"Word being trained: {args.word}, Word Embeddings being used: {args.word_embeddings}")
+
+            training_set = DatasetLoader(args.word, "train")
+            training_generator = torch.utils.data.DataLoader(training_set, **params)
+
+            criterion = torch.nn.BCELoss(reduction="mean")
+
+            model = LogisticRegression(num_features, 1)
+            
+            optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
 
 
-        use_cuda = torch.cuda.is_available()
-        device = torch.device("cuda:0" if use_cuda else "cpu")
-        # torch.backends.cudnn.benchmark = True
+            use_cuda = torch.cuda.is_available()
+            device = torch.device("cuda:0" if use_cuda else "cpu")
+            # torch.backends.cudnn.benchmark = True
 
-        model.to(device)
+            model.to(device)
 
-        loss_values = []
-        for epoch in range(max_epochs):
-            # Training
-            running_loss = 0.0
+            loss_values = []
+            for epoch in range(max_epochs):
+                # Training
+                running_loss = 0.0
 
-            for i, data in enumerate(training_generator, 0):
-                # data is a list of [features, labels]
-                features, labels = data
-                # Transfer to GPU
-                features, labels = features.to(device), labels.to(device)
+                for i, data in enumerate(training_generator, 0):
+                    # data is a list of [features, labels]
+                    features, labels = data
+                    # Transfer to GPU
+                    features, labels = features.to(device), labels.to(device)
 
-                # zero the parameter gradients
-                optimizer.zero_grad()
+                    # zero the parameter gradients
+                    optimizer.zero_grad()
 
-                # Model computations
-                model.train() # Take this out
-                
-                # forward + backward + optimize
-                predictions = model(features)
-                # Compute Loss
-                loss = criterion(predictions, labels)
-                # Backward pass
-                loss.backward()
-                optimizer.step()
-                
-                running_loss += loss.item() * features.size(0)
+                    # Model computations
+                    model.train() # Take this out
+                    
+                    # forward + backward + optimize
+                    predictions = model(features)
+                    # Compute Loss
+                    loss = criterion(predictions, labels)
+                    # Backward pass
+                    loss.backward()
+                    optimizer.step()
+                    
+                    running_loss += loss.item() * features.size(0)
 
-            loss_values.append(running_loss / len(training_generator))
-            writer.add_scalar(f'{word}: training loss', running_loss / len(training_generator), epoch)
-                
-                # if i % 100 == 99:    # every 100 mini-batches...
+                loss_values.append(running_loss / len(training_generator))
+                writer.add_scalar(f'{word}: training loss', running_loss / len(training_generator), epoch)
+                    
 
-                #     # ...log the running loss
-                #     writer.add_scalar(f'{word}: training loss',
-                #                     running_loss / 1000,
-                #                     epoch * len(training_generator) + i)
+            # Tests and Accuracies
+            x_data, y_data, _ = data_loader(args.word)
 
-                #     # ...log a Matplotlib Figure showing the model's predictions on a
-                #     # random mini-batch
+            writer.add_graph(model, x_data)
 
-                #     writer.add_figure(f'{word}: predictions vs. actuals',
-                #                 plot_classes_preds(model, features, labels),
-                #                 global_step=epoch * len(training_generator) + i)
-                #     running_loss = 0.0
+            writer.add_figure("Learning Rate", plot_learning_rate(loss_values))
 
-            # # Validation
-            # with torch.set_grad_enabled(False):
-            #     for features, labels in validation_generator:
-            #         # Transfer to GPU
-            #         features, labels = features.to(device), labels.to(device)
+            scores = evaluate(model, x_data, y_data)
 
-            #         # Model computations
-            #         [...]
+            writer.add_text(f"{word} Accuracy", str(scores["_accuracy"]))
+            writer.add_text(f"{word} F1 Score", str(scores["_f1_score"]))
+            writer.add_text(f"{word} roc_auc_score", str(scores["_roc_auc_score"]))
+            writer.add_text(f"{word} fpr", str( scores["fpr"] ))
+            writer.add_text(f"{word} tpr", str( scores["tpr"] ))
+            writer.add_text(f"{word} average thresholds", str( scores["thresholds"] ))
+            writer.add_text(f"{word} _auc", str(scores["_auc"]))
+            writer.add_text(f"{word} Precision", str( scores["precision"] ))
+            writer.add_text(f"{word} Recall", str( scores["recall"] ))
 
-
-        # Loop over epochs
-        # train_loss = []
-        # for epoch in range(max_epochs):
-        #     # Training
-        #     running_loss = 0.0
-        #     for local_batch, local_labels in training_generator:
-        #         # Transfer to GPU
-        #         local_batch, local_labels = local_batch.to(device), local_labels.to(device)
-
-        #         # Model computations
-        #         model.train()
-        #         # Forward pass
-        #         y_pred = model(local_batch)
-        #         # Compute Loss
-        #         loss = criterion(y_pred, local_labels)
-        #         train_loss.append(loss.item())
-
-        #         optimizer.zero_grad()
-        #         # Backward pass
-        #         loss.backward()
-        #         optimizer.step()
-
-        #         running_loss += loss.item() * local_batch.size(0)
-        #     epoch_loss = running_loss / len(local_batch)
-        #     train_loss.append(epoch_loss)
-
-        
-
-        # Tests and Accuracies
-        x_data, y_data, _ = data_loader(args.word)
-
-        writer.add_graph(model, x_data)
-
-        writer.add_figure("Learning Rate", plot_learning_rate(loss_values))
-
-        scores = evaluate(model, x_data, y_data)
-
-        writer.add_text(f"{word} Accuracy", str(scores["_accuracy"]))
-        writer.add_text(f"{word} F1 Score", str(scores["_f1_score"]))
-        writer.add_text(f"{word} roc_auc_score", str(scores["_roc_auc_score"]))
-        writer.add_text(f"{word} fpr", str( scores["fpr"] ))
-        writer.add_text(f"{word} tpr", str( scores["tpr"] ))
-        writer.add_text(f"{word} average thresholds", str( scores["thresholds"] ))
-        writer.add_text(f"{word} _auc", str(scores["_auc"]))
-        writer.add_text(f"{word} Precision", str( scores["precision"] ))
-        writer.add_text(f"{word} Recall", str( scores["recall"] ))
-
-        writer.add_figure(
-            f"{word} False Positive Rate", plot_tpr(scores["fpr"], scores["tpr"], args.word)
-        )
-        writer.add_figure(
-            f"{word} Precision vs Recall",
-            plot_precision_recall(scores["recall"], scores["precision"], args.word),
-        )
+            writer.add_figure(
+                f"{word} False Positive Rate", plot_tpr(scores["fpr"], scores["tpr"], args.word)
+            )
+            writer.add_figure(
+                f"{word} Precision vs Recall",
+                plot_precision_recall(scores["recall"], scores["precision"], args.word),
+            )
 
     logger.print_time()
     writer.close()
@@ -243,6 +203,13 @@ if __name__ == "__main__":
         type=boolean_string,
         default=True,
         help="If set to true, training is done on entire dataset. Default: True" 
+    )
+    parser.add_argument(
+        "-ae",
+        "--all_embeddings",
+        type=boolean_string,
+        default=False,
+        help="If set to true, training is done on all word embeddings. Default: False" 
     )
     parser.add_argument(
         "-lr",
